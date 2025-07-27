4420cf15d4e9261663e514ef7e52adffa0a906b9
Haim Assraf <haimassraf@gmail.com>
2025-07-27 12:52:54 +0300

    f

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -2,10 +2,8 @@ import express from 'express';
 import { login } from '../auth/login.js';
 import { signup } from '../auth/signup.js';
 import { logout } from '../auth/logout.js';
-import { authMiddleware } from '../auth/authMiddleware.js';
 
 const authRouter = express.Router();
-
 
 authRouter.post('/login', login);
 authRouter.post('/signup', signup);
